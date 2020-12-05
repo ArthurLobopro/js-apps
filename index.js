1caea4a1a3f2106492df28defc630799a6c708d6
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-05 08:45:28 -0300

    Atualização dos arquivos index e coreção de bugs

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
 var travado =false
-function troca(){
-    travado = (travado===true)? false : true
+var content = document.getElementById("content")
+function escreve(nome){
+    switch(nome){
+        case "area":
+            if(travado===false){
+                escreve_area()
+            }
+            break
+        case "binario":
+            if(travado===false){
+                escreve_binario()
+            }
+            break
+    }
+}
+function trava(nome){
+    if(travado==false){
+        switch(nome){
+            case "area":
+                escreve_area()
+                break
+            case "binario":
+                escreve_binario()
+                break
+        }
+        travado=true
+    }else{
+        travado=false
+    }
+
+}
+function destrava(){
+    travado=false
+}
+function escreve_area(){
+    let res=`<object data="area/area.html" type="text/html" width="99%"  height="99%" ></object>`
+    content.innerHTML=res
+    document.body.style.minWidth="955px"
+}
+function escreve_binario(){
+    let res = `<object data="binario/binario.html" type="text/html" width="99%"  height="99%" ></object>`
+    content.innerHTML=res
+    document.body.style.minWidth="955px"
 }
