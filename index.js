92d1bdd2016840820a4b47d6a05f2a3adecd9d2a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-05 11:48:56 -0300

    Atualizações de funções e pequenas correções

--- index.js
+++ index.js
@@ -12,23 +12,49 @@ function escreve(nome){
                 escreve_binario()
             }
             break
+        case "distancia":
+            if(travado===false){
+                escreve_distancia()
+            }
+            break
     }
 }
+var nome_backup 
 function trava(nome){
-    if(travado==false){
-        switch(nome){
-            case "area":
-                escreve_area()
-                break
-            case "binario":
-                escreve_binario()
-                break
-        }
-        travado=true
-    }else{
-        travado=false
-    }
     
+    if(nome_backup==nome){
+        travado=(!travado)
+    }else{
+        if(travado==false){
+            switch(nome){
+                case "area":
+                    escreve_area()
+                    break
+                case "binario":
+                    escreve_binario()
+                    break
+                case "distancia":
+                    escreve_distancia()
+                    break
+            }
+            travado=true
+        }else{
+            travado=false
+            switch(nome){
+                case "area":
+                    escreve_area()
+                    break
+                case "binario":
+                    escreve_binario()
+                    break
+                case "distancia":
+                    escreve_distancia()
+                    break
+            }
+            travado=true
+        }
+    }
+    nome_backup=nome
 }
 function destrava(){
     travado=false
@@ -36,7 +62,7 @@ function destrava(){
 var nome= document.getElementById("nome")
 
 function escreve_area(){
-    let res=`<object data="./area/area.html" type="text/html" width="99%"  height="99%" ></object>`
+    let res=`<object data="./area/area.html" type="text/html" width="100%"  height="100%" ></object>`
     content.innerHTML=res
     document.body.style.minWidth="955px"
     nome.innerText="Calculador de Área"
@@ -44,11 +70,19 @@ function escreve_area(){
     mostra_msg()
 }
 function escreve_binario(){
-    let res = `<object data="./binario/binario.html" type="text/html" width="99%"  height="99%" ></object>`
+    let res = `<object data="./binario/binario.html" type="text/html" width="100%"  height="100%" ></object>`
     content.innerHTML=res
     document.body.style.minWidth="955px"
     nome.innerText="Conversor Binário"
     nome.href="./binario/binario.html"
+    mostra_msg()
+}
+function escreve_distancia(){
+    let res = `<object data="./distancia/distancia.html" type="text/html" width="100%"  height="100%" ></object>`
+    content.innerHTML=res
+
+    nome.innerText="Conversor de Distância"
+    nome.href="./distancia/distancia.html"
     mostra_msg()
 }
 function mostra_msg(){
