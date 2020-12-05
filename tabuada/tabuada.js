2c4adabd4a1b21bd71db57b65dbb1c4312fff34b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-05 15:41:40 -0300

    Programa Gerador de Tabuada

---
+++ tabuada/tabuada.js
@@ -0,0 +1,27 @@
+function gerar(){
+    let num=document.getElementById("num")
+    let res=document.getElementById("res")
+    let inicio=document.getElementById("inicio")
+    let fim=document.getElementById("fim")
+    let resposta=""
+    if(num.value.length == 0){
+        alert("Por favor insira um número para continuar")
+    }else if(inicio.value.length == 0 || fim.value.length == 0){
+        alert("Por favor insira um início e fim para continuar")
+    }else{
+        num=Number(num.value)
+        inicio=Number(inicio.value)
+        fim=Number(fim.value)
+        if(inicio>fim){
+            let ajudante=fim
+            fim=inicio
+            inicio=ajudante
+        }
+        resposta+=`<div class="res">`
+        for(let i=inicio;i<=fim;i++){
+            resposta+=`${num} x ${i} = ${num*i}<br>`
+        }
+        resposta+=`<div>`
+        res.innerHTML+=resposta
+    }
+}
