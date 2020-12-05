cf04a488fc58cdd4f9207cc8bdfeb4fc4a4820cf
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-05 10:56:30 -0300

    Atualização de hiperlinks

--- index.js
+++ index.js
@@ -28,18 +28,30 @@ function trava(nome){
     }else{
         travado=false
     }
-
+    
 }
 function destrava(){
     travado=false
 }
+var nome= document.getElementById("nome")
+
 function escreve_area(){
-    let res=`<object data="area/area.html" type="text/html" width="99%"  height="99%" ></object>`
+    let res=`<object data="./area/area.html" type="text/html" width="99%"  height="99%" ></object>`
     content.innerHTML=res
     document.body.style.minWidth="955px"
+    nome.innerText="Calculador de Área"
+    nome.href="./area/area.html"
+    mostra_msg()
 }
 function escreve_binario(){
-    let res = `<object data="binario/binario.html" type="text/html" width="99%"  height="99%" ></object>`
+    let res = `<object data="./binario/binario.html" type="text/html" width="99%"  height="99%" ></object>`
     content.innerHTML=res
     document.body.style.minWidth="955px"
+    nome.innerText="Conversor Binário"
+    nome.href="./binario/binario.html"
+    mostra_msg()
+}
+function mostra_msg(){
+    let msg= document.getElementById("msg")
+    msg.style.visibility="visible"
 }
