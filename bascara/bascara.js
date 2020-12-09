17695c38fe41d3b180b470425329a13e2fd96e89
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-08 21:32:10 -0300

    Calculador Equação de 2 grau Update

--- bascara/bascara.js
+++ bascara/bascara.js
@@ -1,3 +1,5 @@
+var res = document.getElementById("res")
+var id=0
 function calcular(){
     let a=Number(document.getElementById("a").value)
     let b=Number(document.getElementById("b").value)
@@ -7,28 +9,40 @@ function calcular(){
     }else{
         let delta =(b*b)-4*a*c
         let raiz=Math.sqrt(delta)
-        let res=document.getElementById("res")
         let x1,x2
-        document.getElementById("mensagem").style="text-align: center;"
-        document.getElementById("mensagem").innerText="Para limpar as respostas aperte F5 ou recarregue a página"
-        document.getElementById("historico").style="background-color: white;box-shadow: 5px 5px 10px black;display: inherit;"
-    
+        res.style="background-color: white;box-shadow: 5px 5px 10px black;display: inline-block;"
         if (raiz>0){
             x1=(-(b)-raiz)/(a*2)
             x2=(-(b)+raiz)/(a*2)
-            res.innerHTML+=`<div class="res">A: ${a}<br>B: ${b}<br>C: ${c}<br>Δ= ${delta}<br>Raiz: ${raiz}<br>X: ${x1} ou ${x2} <div>`   
+            res.innerHTML+=`<div class="res" id="div${id}">
+            <div class="circle"><img src="../midia/close-icon.png" onclick="remove_div(${id})"></div>
+            A: ${a}<br>B: ${b}<br>C: ${c}<br>Δ= ${delta}<br>Raiz: ${raiz}<br>X: ${x1} ou ${x2} <div>`   
         }else if(raiz==0){
             x1=-(b)/(a*2)
-            res.innerHTML+=`<div class="res">A: ${a}<br>B: ${b}<br>C: ${c}<br>Δ= ${delta}<br>Raiz: ${raiz}<br>X: ${x1}`
+            res.innerHTML+=`<div class="res" id="div${id}">
+            <div class="circle"><img src="../midia/close-icon.png" onclick="remove_div(${id})"></div>
+            A: ${a}<br>B: ${b}<br>C: ${c}<br>Δ= ${delta}<br>Raiz: ${raiz}<br>X: ${x1}`
         }else{
-            res.innerHTML+=`<div class="res">A: ${a}<br>B: ${b}<br>C: ${c}<br>Δ= ${delta}<br>Raiz:Inexistente<br>X: Inexistente <div>`
+            res.innerHTML+=`<div class="res" id="div${id}">
+            <div class="circle"><img src="../midia/close-icon.png" onclick="remove_div(${id})"></div>
+            A: ${a}<br>B: ${b}<br>C: ${c}<br>Δ= ${delta}<br>Raiz:Inexistente<br>X: Inexistente <div>`
         }
+        id++
         zerar()
     }
-    
 }
 function zerar(){
     document.getElementById("a").value="0"
     document.getElementById("b").value="0"
     document.getElementById("c").value="0"
+}
+function remove_div(num){
+    let element= document.getElementById(`div${num}`)
+    res.removeChild(element)
+    let string = res.innerHTML
+    let teste = string.indexOf("div")
+    if(teste==-1){
+         res.style.display="none"
+         id=0
+    }
 }
