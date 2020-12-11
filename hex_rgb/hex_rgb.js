d9a4907b2bc17df026ea0a888297fe40f03c2af5
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-11 00:39:21 -0300

    Conversor HEX/RGB Update

--- hex_rgb/hex_rgb.js
+++ hex_rgb/hex_rgb.js
@@ -24,11 +24,108 @@ function convert_hex(){
         id++
         res.innerHTML+=content
         res.style.display="inline-block"
-        zerar()
+        zerar(1)
     }
 }
-function zerar(){
-    document.getElementById("hex").value=""
+function convert_rgb(){
+    let r = Number(document.getElementById("r").value)
+    let g = Number(document.getElementById("g").value)
+    let b = Number(document.getElementById("b").value)
+   if((r>=0 && r<=255) && (g>=0 && g<=255) && (b>=0 && b<=255)){
+        let hex =(`#${String(acha_hex(r)+acha_hex(g)+acha_hex(b)).toUpperCase()}`)
+        let content = ` <div class="res" id="div${id}">
+                            <div class="circle" onclick="remove_div(${id})"><img src="../midia/close-icon.png"></div>
+                            <div class="color" style="background-color: ${hex};"></div><br>
+                            RGB: (${r}, ${g}, ${b})<br>
+                            HEX: ${hex}<br>
+                        <div>`
+        id++
+        res.innerHTML+=content
+        res.style.display="inline-block"
+        zerar(2)
+   }else{
+       alert("Você informou algum número inválido, verifique as informações e tente novamente!")
+   }
+    
+}
+function acha_hex(num){
+    for(i=0;i<16;i++){
+        for(j=0;j<16;j++){
+            if((i*16+j*1)==num){
+                if(i>=0 && i<=9){
+                    i=String(i)
+                }else{
+                    switch(i){
+                        case 10:
+                            i="a"
+                            break
+                        case 11:
+                            i="b"
+                            break
+                        case 12:
+                            i="c"
+                            break
+                        case 13: 
+                            i="d"
+                            break
+                        case 14:
+                            i="e"
+                            break
+                        case 15:
+                            i="f"
+                            break                           
+                    }
+                }
+                if(j>=0 && j<=9){
+                    j=String(j)
+                }else{
+                    switch(j){
+                        case 10:
+                            j="a"
+                            break
+                        case 11:
+                            j="b"
+                            break
+                        case 12:
+                            j="c"
+                            break
+                        case 13: 
+                            j="d"
+                            break
+                        case 14:
+                            j="e"
+                            break
+                        case 15:
+                            j="f"
+                            break                           
+                    }
+                }
+                return String(i+j)
+            }
+        }
+    }
+}
+function acha_nome(){
+    let hex_string = document.getElementById("nome").value
+    let hex = []
+        hex_string=hex_string.toUpperCase()
+        for(i=0;i<6;i++){
+            hex[i]=hex_string[i]
+            hex[i]=convert_hex_to_num(hex[i])
+        }
+        let r = hex[0]*16 + hex[1]*1
+        let g = hex[2]*16 + hex[3]*1
+        let b = hex[4]*16 + hex[5]*1
+        let rgb = `${r}, ${g}, ${b}`
+        let content = ` <div class="res" id="div${id}">
+                            <div class="circle" onclick="remove_div(${id})"><img src="../midia/close-icon.png"></div>
+                            <div class="color" style="background-color: rgb(${rgb});"></div><br>
+                            HEX: #${hex_string}<br>
+                            RGB: (${rgb})
+                        <div>`
+        id++
+        res.innerHTML+=content
+        res.style.display="inline-block"
 }
 //Converte hexadecimal para números
 function convert_hex_to_num(a){
@@ -74,5 +171,14 @@ function remove_div(num){
     if(teste==-1){
         res.style.display="none"
         id=0
+    }
+}
+function zerar(num){
+    if(num==1){
+        document.getElementById("hex").value=""
+    }else{
+        document.getElementById("r").value=""
+        document.getElementById("g").value=""
+        document.getElementById("b").value=""
     }
 }
