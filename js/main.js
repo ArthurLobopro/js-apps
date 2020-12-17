40831e8ec38cd15d3a475b10e6715c1c5eb0dad7
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-17 16:16:25 -0300

    Add main js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let id_button = `id="submit-button"`
+
+function remove_div(num){
+    let element= document.getElementById(`div${num}`)
+    res.removeChild(element)
+    let string = res.innerHTML
+    let teste = string.indexOf("div")
+    if(teste==-1){
+        res.style.display="none"
+        id=0
+    }
+}
+function auto_submit(event){
+    let tecla = event.key
+    if(tecla=="Enter"){
+        document.getElementById("submit-button").click()
+    }
+}
