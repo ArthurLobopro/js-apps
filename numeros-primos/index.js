c99eb5dfc29eb99a3979a355e0c7c64d64f22dce
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-25 23:04:22 -0300

    gerador de números primos usando módulos

--- numeros-primos/index.js
+++ numeros-primos/index.js
@@ -1,29 +1,32 @@
+import { addEvent, circle, id, range } from "../public/js/modules.js";
 const res = document.getElementById('res')
-let min = 0 
-let max = 100
-let primos
-const range = (min,max,pass=1) => {
-    let array = []
-    for(i = min;i<max;i+=pass){ array.push(i) }
-    return array
-}
+const inicio = document.getElementById('inicio')
+const fim = document.getElementById('fim')
 const isPrimo = (n) => {
-    divs = 0    
-    for(i of range(1,n+1)){ divs+= (n % i == 0)? 1 : 0 }
+    let divs = 0    
+    for(let i of range(1,n+1)){ divs+= (n % i == 0)? 1 : 0 }
     return divs == 2 ? true : false
 }
 function gerar() {
-    let inicio = Number(document.getElementById('inicio').value)
-    let fim = Number(document.getElementById('fim').value)
-    primos = []
-    for( i of range(inicio,fim)){
+    let start = Number(inicio.value)
+    let end = Number(fim.value)
+    let primos = []
+    for(let i of range(start,end)){
         if(isPrimo(i)){ primos.push(i) }
     }
     let content = `
-    <div class='res'>
-        Intervalo: ${inicio}-${fim}<br>
+    <div class='res' id="div${id.id}">
+        ${circle(id.id)}
+        Intervalo: ${start}-${end}<br>
         Sequência: ${primos.join(', ')}.
     </div>`
     res.innerHTML+=content
     res.style.display='flex'
+    addEvent()
+    id.increase()
 }
+const submit_button = document.getElementById("submit")
+submit_button.onclick = gerar
+const auto_submit = event => { if(event.key == "Enter"){ gerar() } }
+inicio.onkeydown = auto_submit
+fim.onkeydown = auto_submit
