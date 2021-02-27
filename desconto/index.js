a08e5c23ac64445d96bdcf8ebac9cc1d151bd11f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-26 21:57:30 -0300

    Calculadora de desconto usando módulos

--- desconto/index.js
+++ desconto/index.js
@@ -1,7 +1,7 @@
+import { addEvent,circle, id } from "../public/js/modules.js"
 const res = document.getElementById('res')
 const num = document.getElementById('num')
 const desc = document.getElementById('desc')
-var id = 0
 function desconto(){
     if(num.value.length==0 || desc.value.length == 0){
         alert('Informe o valor e o desconto para prosseguir.')
@@ -11,13 +11,21 @@ function desconto(){
         let total = n
         total-= n*d/100
         let content = `
-        <div class='res' id='div${id}'>
-            <div class="circle" onclick="remove_div(${id})"><img src="../public/midia/close-icon.png"></div>
+        <div class='res' id='div${id.id}'>
+            ${circle(id.id)}
             Preço: R$${n.toFixed(2)} <br>
             Desconto: ${d}% <br>
             Total: R$${total.toFixed(2)}
         </div>`
         res.innerHTML+=content
-        res.style.display='inline-block'
+        res.style.display='flex'
+        addEvent()
+        num.value=''
+        desc.value=''
+        id.increase()
     }
 }
+const submit_button = document.getElementById("submit-button")
+submit_button.onclick = desconto
+num.onkeydown = event => { if(event.key == "Enter"){ desc.focus() } }
+desc.onkeydown = event => { if(event.key == "Enter"){ desconto() } }
