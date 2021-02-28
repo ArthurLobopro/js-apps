0b787e9870c22af3d2205956a20841ad3b0359b5
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-02-28 13:09:00 -0300

    Uso de módulos

--- area/index.js
+++ area/index.js
@@ -1,19 +1,19 @@
-const get = id => document.getElementById(id)
+import { addEvent,circle, id ,get } from "../public/js/modules.js";
 const res= get("res")
 let medida = "mm"
-var id = 0
-const functions = {
-    contrutorRes(nome,string,largura=50,infoAdImg=""){
-        res.innerHTML+=`<div class="res" id="div${id}">
-        <div class="circle" onclick="remove_div(${id})"><img src="../public/midia/close-icon.png"></div>
-        <div class="ac"><img src="img/${nome}.png" width="${largura}px" heigth="50px" ${infoAdImg}></div>
-        ${string}<sup>2</sup></div>`
+function contrutorRes(nome,string,largura=50,infoAdImg=""){
+        res.innerHTML+=`
+        <div class="res" id="div${id.id}">
+            ${circle(id.id)}
+            <div class="ac"><img src="img/${nome}.png" width="${largura}px" heigth="50px" ${infoAdImg}></div>
+            ${string}<sup>2</sup>
+        </div>`
         res.style.display='flex'
-        id++
+        addEvent()
+        id.increase()
         zerar(nome)
-    }
 }
-const construtorInput = nome =>{
+const construtorInput = nome => {
     const inputs = document.querySelectorAll("#input-types > div")
     get("input").style.display=""
     for(let i of inputs){
@@ -30,32 +30,32 @@ const calcular = {
     quadrado: () => {
         let lado = Number(get("lado").value)
         let string = `Lado: ${lado+medida}<br>Área: ${(lado**2)+medida}`
-        functions.contrutorRes('quadrado',string)
+        contrutorRes('quadrado',string)
     },
     triangulo: () => {
         let base = Number(get("tri-base").value)
         let altura = Number(get("tri-altura").value)
         let string = `Base: ${base + medida}<br>Altura: ${altura + medida}<br>Área: ${(base*altura/2) + medida}`
-        functions.contrutorRes('triangulo',string)
+        contrutorRes('triangulo',string)
     },
     retangulo: () => {
         let base=Number(get("ret-base").value)
         let altura=Number(get("ret-altura").value)
         let string = `Base: ${base+medida}<br>Altura: ${altura+medida}<br>Área: ${(base*altura)+medida}`
-        functions.contrutorRes('retangulo',string,80,'style="margin: 2px 0 auto -18px;"')
+        contrutorRes('retangulo',string,80,'style="margin: 2px 0 auto -18px;"')
     },
     circulo: () =>{
         let raio=Number(get("raio").value)
         let pi=String(get("pi").value).replace(",", ".")
         pi=Number(pi)
         let string = `Valor de Pi: ${pi}<br>Raio: ${raio+medida}<br>Área: ${(pi*raio**2)+medida}`
-        functions.contrutorRes('circulo',string,50,'style="width:55px;"')
+        contrutorRes('circulo',string,50,'style="width:55px;"')
     },
     losango: () => {
         let d=Number(get("d").value)
         let D=Number(get("D").value)
         let string = `D: ${D+medida}<br>d: ${d+medida}<br>Área: ${(d*D/2)+medida}`
-        functions.contrutorRes('losango',string)
+        contrutorRes('losango',string)
     },
 }
 function zerar(forma){
