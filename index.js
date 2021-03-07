c80c3ddb8306591f851348ed338646f0302ccce0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-03-07 00:49:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -119,3 +119,18 @@ for(let i of list){
 for(let i of list){
     i.ondblclick = trava
 }
+// Adição de elementos em segundo plano
+setTimeout(() => {
+    const itens = [
+        "area","hip","media","bases","conversorImg","desconto","distancia","rgb_hex","romano","velocidade","bascara","potencia","tabuada","picker"
+    ]
+    itens.forEach((value) => {
+        const iframe = document.createElement("iframe")
+        iframe.src = functions[value].caminho
+        iframe.id = value
+        iframe.style.display="none"
+        if(get(value) == undefined){
+            content.appendChild(iframe)
+        }
+    })
+}, 2000)
