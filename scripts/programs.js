4c551f8eddaaa4b9b1ba5cf90d6432c5573a06ac
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-04-16 10:23:38 -0300

    Módulos criados e adaptações feitas

---
+++ scripts/programs.js
@@ -0,0 +1,59 @@
+const programs = {
+    area:{
+        caminho: "area/",
+        text: 'Calculador de Área'
+    },
+    hip:{
+        caminho: "hipotenusa/",
+        text: "Calculador de Hipotenusa"
+    },
+    media:{
+        caminho: "media/",
+        text: "Calculadora de Média"
+    },
+    bases:{
+        caminho: "bases/",
+        text: "Conversor de  Bases"
+    },
+    conversorImg:{
+        caminho: 'conversor-img/',
+        text: 'Conversor de Imagens'
+    },
+    desconto:{
+        caminho: 'desconto/',
+        text: 'Calculadora de Desconto'
+    },
+    distancia:{
+        caminho: "distancia/",
+        text: "Conversor de Distância"
+    },
+    rgb_hex:{
+            caminho: "hex_rgb/",
+            text: "Conversor HEX/RGB"
+    },
+    romano:{
+        caminho: "romano/",
+        text: "Conversor Romano/Decimal"
+    },
+    velocidade:{
+            caminho: "velocidade/",
+            text: "Conversor de Velocidade"
+    },
+    bascara:{
+        caminho: "bascara/",
+        text: "Equação de 2° Grau"
+    },
+    potencia:{
+        caminho: "potencias/",
+        text: "Gerador de Potências"
+    },
+    tabuada:{
+        caminho: "tabuada/",
+        text: "Gerador de Tabuada"
+    },
+    picker:{
+        caminho: "color-picker/",
+        text: "Seletor de cores"
+    }
+}
+export { programs }
