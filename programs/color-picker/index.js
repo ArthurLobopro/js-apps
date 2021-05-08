2ec98ac0ba742bfb44fb9a536b607067a1f1ffb4
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-07 23:05:17 -0300

    correção de bugs

--- programs/color-picker/index.js
+++ programs/color-picker/index.js
@@ -7,6 +7,37 @@ const canvas = get('mycanvas')
 const ctx = canvas.getContext('2d')
 const invi = get('invi')
 const res = get('res')
+
+const readImage = file => {
+    let reader = new FileReader()
+    let img
+    reader.readAsDataURL(file)
+    reader.onerror = () => {
+        alert("Erro ao carregar imagem, tente novamente!")
+    }
+    reader.onload = function (event){ 
+        img = new Image()
+        img.src=event.target.result
+        let w
+        let h
+        img.onload = function() {
+            w= this.width
+            h= this.height
+            //console.log(w + 'x' + h)
+            if(w>500 || h>800){
+                canvas.width = Math.ceil(w/2)
+                canvas.height =  Math.ceil(h/2)
+            }else{
+                canvas.width = w
+                canvas.height = h
+            }
+            ctx.fillStyle='#FFFFFF'
+            ctx.fillRect(0,0,canvas.width,canvas.height)
+            drawImage(img.src,canvas.width,canvas.height)
+        }
+    }
+}
+
 function drawImage(img,w,h){
     let image = new Image()
     image.src=img
@@ -22,40 +53,10 @@ function color(event) {
     let data = pix.data
     return [ data[0], data[1], data[2]]
 } 
-function getImage(file) {
-    file = file ?? upimg.files.item(0)
-    let reader = new FileReader()
-    let img
-    reader.readAsDataURL(file)
-    reader.onprogress = () => {
-        console.log(`Carregados ${event.loaded} de ${event.total}`)
-    }
-    reader.onerror = () => {
-        console.log('erro')
-    }
-    reader.onload = function (event){ 
-        console.log('loaded')
-        img = new Image()
-        img.src=event.target.result
-        let w
-        let h
-        img.onload = function() {
-            w= this.width
-            h= this.height
-            console.log(w + 'x' + h)
-            if(w>500 || h>800){
-                canvas.width = Math.ceil(w/2)
-                canvas.height =  Math.ceil(h/2)
-            }else{
-                canvas.width = w
-                canvas.height = h
-            }
-            ctx.fillStyle='#FFFFFF'
-            ctx.fillRect(0,0,canvas.width,canvas.height)
-            drawImage(img.src,canvas.width,canvas.height)
-        }
-        
-    }
+
+function getImage() {
+    let file = upimg.files.item(0) 
+    readImage(file)
     canvas.style.display='block'
 }
 //Detecção de eventos
@@ -92,7 +93,8 @@ document.onpaste = event => {
     if(files.length >=1){
         let file = files.item(0)
         if(file.type.search("image") != -1){
-            getImage(file)
+            readImage(file)
+            canvas.style.display='block'
         }
     }
 }
