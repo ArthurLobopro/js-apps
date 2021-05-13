85597230aa7695ce0b548bf342e89f16c6f1a153
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-13 00:10:30 -0300

    tipos adicionados

--- scripts/programs.js
+++ scripts/programs.js
@@ -2,72 +2,86 @@ const programs = [
     {
         name: "area",
         caminho: "area/",
-        text: 'Calculador de Área'
+        text: 'Calculador de Área',
+        type: "calculadora"
     },
     {
         name: "hip",
         caminho: "hipotenusa/",
-        text: "Calculador de Hipotenusa"
+        text: "Calculador de Hipotenusa",
+        type: "calculadora"
     },
     {
         name: "media",
         caminho: "media/",
-        text: "Calculadora de Média"
+        text: "Calculadora de Média",
+        type: "calculadora"
     },
     {
         name: "bases",
         caminho: "bases/",
-        text: "Conversor de  Bases"
+        text: "Conversor de  Bases",
+        type: "conversor"
     },
     {
         name: "conversorImg",
         caminho: 'conversor-img/',
-        text: 'Conversor de Imagens'
+        text: 'Conversor de Imagens',
+        type: "conversor"
     },
     {
         name: "desconto",
         caminho: 'desconto/',
-        text: 'Calculadora de Desconto'
+        text: 'Calculadora de Desconto',
+        type: "calculadora"
     },
     {
         name: "distancia",
         caminho: "distancia/",
-        text: "Conversor de Distância"
+        text: "Conversor de Distância",
+        type: "conversor"
     },
     {
         name: "rgb_hex",
         caminho: "hex_rgb/",
-        text: "Conversor HEX/RGB"
+        text: "Conversor HEX/RGB",
+        type: "conversor"
     },
     {
         name: "romano",
         caminho: "romano/",
-        text: "Conversor Romano/Decimal"
+        text: "Conversor Romano/Decimal",
+        type: "conversor"
     },
     {
         name: "velocidade",
         caminho: "velocidade/",
-        text: "Conversor de Velocidade"
+        text: "Conversor de Velocidade",
+        type: "conversor"
     },
     {
         name: "bascara",
         caminho: "bascara/",
-        text: "Equação de 2° Grau"
+        text: "Equação de 2° Grau",
+        type: "outros"
     },
     {
         name: "potencia",
         caminho: "potencias/",
-        text: "Gerador de Potências"
+        text: "Gerador de Potências",
+        type: "outros"
     },
     {
         name: "tabuada",
         caminho: "tabuada/",
-        text: "Gerador de Tabuada"
+        text: "Gerador de Tabuada",
+        type: "outros"
     },
     {
         name: "picker",
         caminho: "color-picker/",
-        text: "Seletor de cores"
+        text: "Seletor de cores",
+        type: "outros"
     }   
 ]
 
@@ -80,4 +94,4 @@ const programsGets = function(programName) {
     })
 }
 
-export { programsGets }
+export { programsGets, programs }
